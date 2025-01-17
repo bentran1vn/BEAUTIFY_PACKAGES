6aae1dbfe49973533e1de19e9e5a6272da2676b9
bentran1vn <tan182205@gmail.com>
2025-01-17 09:12:53 +0700

    persistence

--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.CONTRACT/Services/Users/Responses.cs
+++
@@ -1,6 +0,0 @@
-namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.CONTRACT.Services.Users;
-
-public static class Responses
-{
-    public record UserResponse(Guid Id, string Name);
-}
---
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.PERSISTENCE/DependencyInjection/Options/SqlServerRetryOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.PERSISTENCE.DependencyInjection.Options;
+
+public record SqlServerRetryOptions
+{
+    [Required, Range(5, 20)] public int MaxRetryCount { get; init; }
+    [Required, Timestamp] public TimeSpan MaxRetryDelay { get; init; }
+    public int[]? ErrorNumbersToAdd { get; init; }
+}
