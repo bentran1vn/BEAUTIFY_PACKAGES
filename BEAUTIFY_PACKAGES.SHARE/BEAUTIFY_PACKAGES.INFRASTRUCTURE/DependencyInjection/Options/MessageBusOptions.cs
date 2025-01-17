a89f367031ef899e95403595714b96cd51dbb275
bentran1vn <tan182205@gmail.com>
2025-01-17 16:00:04 +0700

    Add Options Infra

---
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.INFRASTRUCTURE/DependencyInjection/Options/MessageBusOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.INFRASTRUCTURE.DependencyInjection.Options;
+
+public class MessageBusOptions
+{
+    [Required, Range(1, 10)] public int RetryLimit { get; init; }
+    [Required, Timestamp] public TimeSpan InitialInterval { get; init; }
+    [Required, Timestamp] public TimeSpan IntervalIncrement { get; init; }
+}
