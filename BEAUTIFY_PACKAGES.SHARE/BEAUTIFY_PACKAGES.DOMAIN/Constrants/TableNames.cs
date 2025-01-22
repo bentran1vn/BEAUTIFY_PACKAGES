5776ec98190fed21f2607a2cf83b4b85d2bcea8b
bentran1vn <tan182205@gmail.com>
2025-01-23 01:48:22 +0700

    remove redundant tablenames

--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/Constrants/TableNames.cs
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/Constrants/TableNames.cs
@@ -1,8 +1,5 @@
 namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.Constrants;
 public static class TableNames
 {
-    public const string Product = nameof(Product);
-    public const string Event = nameof(Event);
-    public const string Mentor = nameof(Mentor);
-    public const string Slot = nameof(Slot);
+    internal const string Event = nameof(Event);
 }
