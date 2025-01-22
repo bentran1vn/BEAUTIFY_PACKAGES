e536c0cc4e6fdcbe2c268fb447bb87bca04f8b50
bentran1vn <tan182205@gmail.com>
2025-01-23 01:03:55 +0700

    Remove direct exception

--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/Exceptions/BadRequestException.cs
+++
@@ -1,9 +0,0 @@
-namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.Exceptions;
-
-public abstract class BadRequestException : DomainException
-{
-    protected BadRequestException(string message)
-        : base("Bad Request", message)
-    {
-    }
-}
--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/Exceptions/NotFoundException.cs
+++
@@ -1,9 +0,0 @@
-namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.Exceptions;
-
-public abstract class NotFoundException : DomainException
-{
-    protected NotFoundException(string message)
-        : base("Not Found", message)
-    {
-    }
-}
