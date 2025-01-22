66c37219efe01aa3844760f6a8e4c51e4b47869c
bentran1vn <tan182205@gmail.com>
2025-01-22 23:55:05 +0700

    Remove redundant entities in domain

---
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.PERSISTENCE/DependencyInjection/Options/MongoDbSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.PERSISTENCE.DependencyInjection.Options;
+public interface IMongoDbSettings
+{
+    string DatabaseName { get; set; }
+    string ConnectionString { get; set; }
+}
+
+public class MongoDbSettings : IMongoDbSettings
+{
+    public string DatabaseName { get; set; }
+    public string ConnectionString { get; set; }
+}
