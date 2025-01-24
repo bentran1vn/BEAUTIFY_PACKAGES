d77741e24c2dc7d5fa6aaf9660ef879ad1398430
bentran1vn <tan182205@gmail.com>
2025-01-24 14:31:36 +0700

    Remove Exception Redundant

--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/Exceptions/BadRequestException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.Exceptions;
-public abstract class BadRequestException : DomainException
-{
-    protected BadRequestException(string message)
-        : base("Bad Request", message)
-    {
-    }
-}
--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/Exceptions/NotFoundException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.Exceptions;
-public abstract class NotFoundException : DomainException
-{
-    protected NotFoundException(string message)
-        : base("Not Found", message)
-    {
-    }
-}
