13113d760d7ee694889487d4b4bd74abbbe4d8b1
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-02-01 18:19:24 +0700

    Increment version to 1.1.0 and add subscription domain events

---
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.CONTRACT/Services/Subscriptions/DomainEvents.cs
@@ -0,0 +1,37 @@
+﻿using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.CONTRACT.Abstractions.Messages;
+using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.EntityEvents;
+
+namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.CONTRACT.Services.Subscriptions;
+public static class DomainEvents
+{
+    public record SubscriptionCreated(
+        Guid IdEvent,
+        EntityEvent.SubscriptionEntity subscription
+    ) : IDomainEvent, ICommand;
+
+    public record SubscriptionUpdated(
+        Guid IdEvent,
+        EntityEvent.SubscriptionEntity subscription
+    ) : IDomainEvent, ICommand;
+
+    public record SubscriptionDeleted(
+        Guid IdEvent,
+        Guid SubscriptionId
+    ) : IDomainEvent, ICommand;
+
+    public record SubscriptionActivated(
+        Guid IdEvent,
+        Guid SubscriptionId
+    ) : IDomainEvent, ICommand;
+
+    public record SubscriptionDeactivated(
+        Guid IdEvent,
+        Guid SubscriptionId
+    ) : IDomainEvent, ICommand;
+
+    public record SubscriptionPriceUpdated(
+        Guid IdEvent,
+        Guid SubscriptionId,
+        decimal Price
+    ) : IDomainEvent, ICommand;
+}
---
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/EntityEvents/EntityEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.EntityEvents;
+public class EntityEvent
+{
+    public class SubscriptionEntity
+    {
+        private DateTimeOffset Created;
+        private string Description;
+        private int Duration;
+        private Guid Id;
+        private bool IsActivated;
+        private bool IsDeleted;
+        private DateTimeOffset? ModifiedOnUtc;
+        private string Name;
+        private decimal Price;
+    }
+}
