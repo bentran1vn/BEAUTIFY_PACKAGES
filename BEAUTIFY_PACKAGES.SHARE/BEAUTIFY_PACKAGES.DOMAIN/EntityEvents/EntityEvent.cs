07154c5a6fd6b3d0d91a15cb045a3d5935478b8e
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-02-01 19:14:23 +0700

    Refactor SubscriptionEntity fields to improve readability

--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/EntityEvents/EntityEvent.cs
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/EntityEvents/EntityEvent.cs
@@ -3,14 +3,14 @@ public class EntityEvent
 {
     public class SubscriptionEntity
     {
-        private DateTimeOffset Created;
-        private string Description;
-        private int Duration;
-        private Guid Id;
-        private bool IsActivated;
-        private bool IsDeleted;
-        private DateTimeOffset? ModifiedOnUtc;
-        private string Name;
-        private decimal Price;
+        DateTimeOffset Created;
+        string Description;
+        int Duration;
+        Guid Id;
+        bool IsActivated;
+        bool IsDeleted;
+        DateTimeOffset? ModifiedOnUtc;
+        string Name;
+        decimal Price;
     }
 }
