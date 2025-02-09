51f1099935f27ae95ccc0960ce56b7722f705f5c
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-02-09 17:12:45 +0700

    Add CurrentUserService implementation for user context management

---
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.API/DependencyInjection/Extensions/CurrentUserService.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.Abstractions.Repositories;
+using Microsoft.AspNetCore.Http;
+
+namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.API.DependencyInjection.Extensions;
+public class CurrentUserService(IHttpContextAccessor httpContextAccessor) : ICurrentUserService
+{
+    private readonly ClaimsPrincipal? _claimsPrincipal = httpContextAccessor?.HttpContext?.User;
+
+    public string? UserId => _claimsPrincipal?.FindFirstValue("UserId");
+    public string? Role => _claimsPrincipal?.FindFirstValue(ClaimTypes.Role);
+
+    public string? UserName => _claimsPrincipal?.FindFirstValue(ClaimTypes.Name);
+
+}
