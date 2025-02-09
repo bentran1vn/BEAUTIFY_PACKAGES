7a9d5e8c720725946e0d683f0490c1cd5654a73b
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-02-09 18:38:03 +0700

    Add Constant class for role definitions and increment version to 1.2.5

---
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/Constrants/Constant.cs
@@ -0,0 +1,10 @@
+﻿namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.Constrants;
+public static class Constant
+{
+    public const string DOCTOR = "Doctor";
+    public const string CUSTOMER = "Customer";
+    public const string CLINIC = "Clinic";
+    public const string ADMIN = "Admin";
+    public const string SYSTEM = "System";
+    public const string STAFF = "Staff";
+}
