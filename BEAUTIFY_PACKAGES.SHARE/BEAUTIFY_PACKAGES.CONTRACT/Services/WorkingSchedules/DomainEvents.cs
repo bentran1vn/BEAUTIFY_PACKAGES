2e4b338b2d50872be4a2e6a3e761a404357ee709
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-02-17 15:56:20 +0700

    Add WorkingScheduleDeleted and WorkingScheduleUpdated records; bump version to 1.3.3

--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.CONTRACT/Services/WorkingSchedules/DomainEvents.cs
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.CONTRACT/Services/WorkingSchedules/DomainEvents.cs
@@ -7,4 +7,8 @@ public static class DomainEvents
     public record WorkingScheduleCreated(
         Guid IdEvent,
         List<EntityEvent.WorkingScheduleEntity> WorkingScheduleEntities) : IDomainEvent,ICommand;
+    
+    public record WorkingScheduleDeleted(Guid IdEvent,Guid WorkingId) : IDomainEvent,ICommand;
+    
+    public record WorkingScheduleUpdated(Guid IdEvent,List<EntityEvent.WorkingScheduleEntity> WorkingScheduleEntities) : IDomainEvent,ICommand;
 }
