7de5a7a6eb80f7803517a27ab4c137896ca05686
bentran1vn <tan182205@gmail.com>
2025-02-19 00:53:59 +0700

    New Create Service Event

--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/EntityEvents/ClinicServiceEvent.cs
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/EntityEvents/ClinicServiceEvent.cs
@@ -5,10 +5,10 @@ public static class ClinicServiceEvent
     public record CreateClinicService(
         Guid Id, string Name, string Description,
         string[] CoverImage, string[] DescriptionImage,
-        decimal Price, Category Category, Clinic Clinic);
+        decimal Price, Category Category, ICollection<Clinic> Clinic);
     
     public record Category(Guid Id, string Name, string Description);
     
     public record Clinic(Guid Id, string Name, string Email, string Address,
-        string PhoneNumber, string? ProfilePictureUrl);
+        string PhoneNumber, string? ProfilePictureUrl, bool? IsParent, Guid? ParentId);
 }
