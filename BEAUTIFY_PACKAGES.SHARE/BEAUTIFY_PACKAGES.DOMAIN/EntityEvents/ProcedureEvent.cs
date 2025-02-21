21d2339b3f70c6b3ace72cb27f2293b3e564b285
bentran1vn <tan182205@gmail.com>
2025-02-22 00:55:23 +0700

    Change Procedure Create Event

--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/EntityEvents/ProcedureEvent.cs
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/EntityEvents/ProcedureEvent.cs
@@ -4,6 +4,7 @@ public class ProcedureEvent
 {
     public record CreateProcedure(Guid Id, Guid ServiceId, string Name,
         string Description, decimal MaxPrice, decimal MinPrice,
+        decimal? DiscountMaxPrice, decimal? DiscountMinPrice,
         int StepIndex, string[] coverImage, ICollection<ProcedurePriceType> procedurePriceTypes
     );
 
