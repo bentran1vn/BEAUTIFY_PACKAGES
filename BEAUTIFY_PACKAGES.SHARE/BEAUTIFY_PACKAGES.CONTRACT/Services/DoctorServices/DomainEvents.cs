436c23b0f52abb35e895cec9e30c45727ce194da
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-02-25 01:12:32 +0700

    Update DoctorServiceCreated event to accept a list of DoctorServiceEntity; bump version to 1.4.4

--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.CONTRACT/Services/DoctorServices/DomainEvents.cs
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.CONTRACT/Services/DoctorServices/DomainEvents.cs
@@ -4,7 +4,7 @@ using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.EntityEvents;
 namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.CONTRACT.Services.DoctorServices;
 public static class DomainEvents
 {
-    public record DoctorServiceCreate(Guid IdEvent, EntityEvent.DoctorServiceEntity entity) : IDomainEvent, ICommand;
+    public record DoctorServiceCreated(Guid IdEvent, List<EntityEvent.DoctorServiceEntity> entity) : IDomainEvent, ICommand;
 
     public record DoctorServiceDeleted(Guid IdEvent, List<Guid> DoctorServiceIds) : IDomainEvent, ICommand;
 }
