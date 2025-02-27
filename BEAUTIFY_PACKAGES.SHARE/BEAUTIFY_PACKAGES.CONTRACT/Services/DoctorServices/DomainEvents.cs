b4449cf89bb5b304f9b59aac166727a11245eee1
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-02-27 16:41:59 +0700

    Update DomainEvents to include ServiceId in DoctorServiceDeleted record

--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.CONTRACT/Services/DoctorServices/DomainEvents.cs
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.CONTRACT/Services/DoctorServices/DomainEvents.cs
@@ -4,7 +4,9 @@ using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.EntityEvents;
 namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.CONTRACT.Services.DoctorServices;
 public static class DomainEvents
 {
-    public record DoctorServiceCreated(Guid IdEvent, List<EntityEvent.DoctorServiceEntity> entity) : IDomainEvent, ICommand;
+    public record DoctorServiceCreated(Guid IdEvent, List<EntityEvent.DoctorServiceEntity> entity)
+        : IDomainEvent, ICommand;
 
-    public record DoctorServiceDeleted(Guid IdEvent, List<Guid> DoctorServiceIds) : IDomainEvent, ICommand;
+    public record DoctorServiceDeleted(Guid IdEvent, Guid ServiceId, List<Guid> DoctorServiceIds)
+        : IDomainEvent, ICommand;
 }
