74d8fd8b54facb6e98b6364634f19cee875fd6ad
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-03-18 02:24:35 +0700

    Refactor CreateProcedure and ProcedurePriceType records for improved readability and structure

--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/EntityEvents/ProcedureEvent.cs
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.DOMAIN/EntityEvents/ProcedureEvent.cs
@@ -1,12 +1,19 @@
 namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.EntityEvents;
-
 public class ProcedureEvent
 {
-    public record CreateProcedure(Guid Id, Guid ServiceId, string Name,
-        string Description, decimal MaxPrice, decimal MinPrice,
-        decimal? DiscountMaxPrice, decimal? DiscountMinPrice,
-        int StepIndex, string[] coverImage, ICollection<ProcedurePriceType> procedurePriceTypes
+    public record CreateProcedure(
+        Guid Id,
+        Guid ServiceId,
+        string Name,
+        string Description,
+        decimal MaxPrice,
+        decimal MinPrice,
+        decimal? DiscountMaxPrice,
+        decimal? DiscountMinPrice,
+        int StepIndex,
+        string[] coverImage,
+        ICollection<ProcedurePriceType> procedurePriceTypes
     );
 
-    public record ProcedurePriceType(Guid Id, string Name, decimal Price);
+    public record ProcedurePriceType(Guid Id, string Name, decimal Price, int Duration, bool IsDefault);
 }
