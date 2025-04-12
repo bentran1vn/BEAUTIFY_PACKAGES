262a2dc20845d902086ed32c4345a1b848bb4ffb
bentran1vn <tan182205@gmail.com>
2025-04-12 13:40:35 +0700

    InActivatedClinicEvent

--- BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.CONTRACT/Services/Clinic/DomainEvents.cs
+++ BEAUTIFY_PACKAGES.SHARE/BEAUTIFY_PACKAGES.CONTRACT/Services/Clinic/DomainEvents.cs
@@ -1,4 +1,5 @@
 ﻿using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.CONTRACT.Abstractions.Messages;
+using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.EntityEvents;
 
 namespace BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.CONTRACT.Services.Clinic;
 public class DomainEvents
@@ -6,5 +7,10 @@ public class DomainEvents
     public record DoctorFromClinicDeleted(
         Guid IdEvent,
         Guid IdDoctor
+    ) : IDomainEvent, ICommand;
+    
+    public record ClinicBranchActivatedAction(
+        Guid IdEvent,
+        ClinicEvent.InActivatedClinic entity
     ) : IDomainEvent, ICommand;
 }
